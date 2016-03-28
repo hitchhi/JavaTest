0e4a59b1443bc35cc47a112a652053d204fd6d52
hitchhi <hitchhi_ke@126.com>
2016-03-28 18:40:01 +0800

    add one line.

--- src/org/jp/testjava/javatest.java
+++ src/org/jp/testjava/javatest.java
@@ -5,6 +5,7 @@ public class javatest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My JavaTest Porject!");
+		System.out.println("My JavaTest Porject!");
 	}
 
 }
