05c115214254f6437c33b9668643dbab6e9ac093
hitchhi <hitchhi_ke@126.com>
2016-03-28 18:16:14 +0800

    Print "My JavaTest Project!"

---
+++ src/org/jp/testjava/javatest.java
@@ -0,0 +1,10 @@
+package org.jp.testjava;
+
+public class javatest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My JavaTest Porject!");
+	}
+
+}
